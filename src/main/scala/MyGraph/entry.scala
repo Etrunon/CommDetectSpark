819506534c5277d48f88bff494de1fe56d481a63
Etrunon <bosottiluca@gmail.com>
2016-12-06 09:04:54 +0100

    done some tests with triangle countings and other functions

--- src/main/scala/MyGraph/entry.scala
+++ src/main/scala/MyGraph/entry.scala
@@ -1,7 +1,7 @@
 package MyGraph
 
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx.Graph
+import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.io.StdIn
 
@@ -11,7 +11,7 @@ import scala.io.StdIn
 object entry {
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setAppName("MyGraph").setMaster("local")
+    val conf = new SparkConf().setAppName("MyGraph").setMaster("local[*]")
 
     val sc = new SparkContext(conf)
     val mygraph = new MyGraph(sc)
@@ -19,9 +19,22 @@ object entry {
     val file = "Data/facebook-cleaned/total.csv"
     val graph = Graph.fromEdgeTuples(mygraph.getEdgeTuples(file), 0)
 
+    //    val maxInDegree: (VertexId, Int)  = graph.inDegrees.reduce(max)
+    //    println(s"MaxDegree: $maxInDegree")
+
+    //    val cc = graph.connectedComponents().vertices
+    //    println(s"ConnectecCOmponents: $cc")
+
+    println("Beginning triangle counting")
+    // Find the triangle count for each vertex
+    val triCounts = graph.triangleCount()
+    for (tri <- triCounts.vertices.take(10)) println(tri)
 
     println("Press any key to exit")
     StdIn.readLine()
-
   }
+
+  //  def max(a: (VertexId, Int), b: (VertexId, Int)): (VertexId, Int) = {
+  //    if (a._2 > b._2) a else b
+  //  }
 }
